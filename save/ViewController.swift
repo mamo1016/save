a86f844674ab30b697918b8aae2df52b2c768097
mamo <rr0061vv@ed.ritsumei.ac.jp>
2017-09-29 14:09:57 +0900

    test

--- save/ViewController.swift
+++ save/ViewController.swift
@@ -1,25 +1,107 @@
-//
-//  ViewController.swift
-//  save
-//
-//  Created by 上田　護 on 2017/09/27.
-//  Copyright © 2017年 上田　護. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate  {
+    
+    @IBOutlet var textField:UITextField!
+    @IBOutlet var label:UILabel!
+    @IBOutlet var label2:UILabel!
 
+    var testText:String = "default"
+    var number: Int!
+    
+    
+    // UserDefaults のインスタンス
+    let userDefaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // textFiel の情報を受け取るための delegate を設定
+        textField.delegate = self
+        
+        // デフォルト値
+        userDefaults.register(defaults: ["DataStore": "default"])
+        userDefaults.register(defaults: ["Data": "default2"])
+        label.text = readData()
+        label2.text = String(readData2())
+        
+//        if number != nil{
+//            number = 1
+//        }else{
+//            number = saveData
+//        }
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{   //textFieldをUITextFieldとして宣言，　Bool型の値を戻す
+        testText = textField.text!
+        
+        label.text = testText
+        
+        // キーボードを閉じる
+        textField.resignFirstResponder()
+        
+        saveData(str: testText)
+        
+        return true
+    }
+    
+    func saveData(str: String){
+        
+        // Keyを指定して保存
+        userDefaults.set(str, forKey: "DataStore")
+        userDefaults.synchronize()
+        print("saveData")
+
+    }
+    
+    func readData() -> String {
+        // Keyを指定して読み込み
+        let str: String = userDefaults.object(forKey: "DataStore") as! String
+        print("readData")
+
+        return (str)
+    }
+    
+    func readData2() -> Int {
+        // Keyを指定して読み込み
+        let str2: Int = userDefaults.object(forKey: "Data") as! Int
+        
+        return (str2)
     }
 
+    
+    
+    @IBAction func buttonTapped(_ sender : AnyObject) { //クリアボタンの処理
+        // Key の値を削除
+        userDefaults.removeObject(forKey: "DataStore")
+        
+        // Keyを指定して読み込み
+        let str: String = userDefaults.object(forKey: "DataStore") as! String
+        
+        label.text = str
+    }
+
+    
+    @IBAction func plus (){
+        if number == nil{
+            number = 1
+        }else{
+            number = number + 1
+        }
+//        label2.text = String(number)
+        
+        userDefaults.set(number, forKey: "Data")
+        let saveData: Int = userDefaults.object(forKey: "Data") as! Int
+        label2.text = String(saveData)
+        print(saveData)
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
