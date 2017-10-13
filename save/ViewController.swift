34e33e7386d13017ba7940f3ede9758e419a8846
mamo1016 <rr0061vv@ed.ritsumei.ac.jp>
2017-10-13 21:57:56 +0900

    xgdg

--- save/ViewController.swift
+++ save/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController, UITextFieldDelegate  {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool{   //textFieldをUITextFieldとして宣言，　Bool型の値を戻す
         testText = textField.text!
-        
+//        dddd
         label.text = testText
         
         // キーボードを閉じる
